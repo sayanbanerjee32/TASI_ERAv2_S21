a0eb6b66c8be27404349f8fbb396361d2cd31078
sayanbanerjee32 <sayanbanerjee32@gmail.com>
2024-06-27 20:58:41 +0530

    added examples and trick to copy model weights

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from huggingface_hub import hf_hub_download
 import joblib
 
 REPO_ID = "sayanbanerjee32/nanogpt2_test"
-data_file = "data/meta.pkl"
 model_file = "saved_model/ckpt.pt"
 
 # load model
@@ -20,6 +19,15 @@ model_spec = torch.load(
     map_location=torch.device('cpu'))
 model_args = model_spec['model_args']
 model_weights = model_spec['model']
+
+# fix the keys of the state dictionary :(
+# honestly no idea how checkpoints sometimes get this prefix, have to debug more
+unwanted_prefix = '_orig_mod.'
+for k,v in list(model_weights.items()):
+    if k.startswith(unwanted_prefix):
+        model_weights[k[len(unwanted_prefix):]] = model_weights.pop(k)
+
+#### load model and weight
 modelconf = GPTConfig(**model_args)
 trained_model = GPT(modelconf)
 trained_model.load_state_dict(model_weights)
@@ -42,10 +50,20 @@ with gr.Blocks() as demo:
     gr.HTML("<h4 align = 'center'> Generate text in the style of William Shakespeare based on an intial text</h4>")
     
     content = gr.Textbox(label = "Enter seed text to generate content")
+    inputs = [
+            content,
+            ]
+    gr.Examples(["Hark, the night doth whisper secrets to the moonlit sea.",
+                "Dost thou perceive the dance of shadows upon yon silent wall?",
+                "In sooth, the heart's desire oft leads astray the wisest soul.",
+                "Lo, a tempest brews within the fairest of skies.",
+                "Methinks the dawn doth paint the heavens with golden fire."], 
+                inputs = inputs)
+    
     with gr.Row():
         mtk = gr.Number(label = "Max tokens to generate", value = 100)
-        tmp = gr.Slider(label = "Temparature (use higher value for higher creativity)", minimum = 0.0, maximum= 1.0,value = 0.7)
-        tn = gr.Number(label="Select Top N in each step (Optional)", value = None)
+        tmp = gr.Slider(label = "Temperature (use higher value for higher creativity)", minimum = 0.0, maximum= 1.0,value = 0.7)
+        tn = gr.Number(label="Select Top N in each step (Optional)", value = 50)
     inputs = [
                 content,
                 mtk, tmp, tn
@@ -54,7 +72,7 @@ with gr.Blocks() as demo:
     outputs  = [gr.Textbox(label = "Generated text")]
     generate_btn.click(fn = generate_text, inputs= inputs, outputs = outputs)
 
-# for collab
+# # for collab
 # demo.launch(debug=True) 
 
 if __name__ == '__main__':
